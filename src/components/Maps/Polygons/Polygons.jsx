8bfd6312b8403ec7c38a2fdecc3cca734e1a65a2
Dmytrii Holiaka <holyaka_d_m@ukr.net>
2024-04-02 17:47:01 +0300

    20240402

--- src/components/Maps/Polygons/Polygons.jsx
+++ src/components/Maps/Polygons/Polygons.jsx
@@ -5,23 +5,44 @@ export const Polygons = () => {
 
   return (
     <>
-      <LayersControl.Overlay name="Canopy height model (CHM)">
-        
+      <LayersControl.Overlay name="Ortomosaic (RGB)">
+        <LayerGroup>
           <TileLayer
-          attribution='&copy; <a href="https://www.researchgate.net/profile/Dmytrii-Holiaka">Dmytrii Holiaka</a> '
-          url="https://raw.githubusercontent.com/holiaka/chornobyl-polygon-tiles/main/rgb-chm/chm-p1/{z}/{x}/{y}.webp"
-          tms="true"
-          minZoom={10}
-          maxZoom={21}
-        />       
+            attribution='&copy; <a href="https://www.researchgate.net/profile/Dmytrii-Holiaka">Dmytrii Holiaka</a> '
+            url="https://raw.githubusercontent.com/holiaka/chornobyl-polygon-tiles/main/orto/orto-p1/{z}/{x}/{y}.webp"
+            tms="true"
+            minZoom={10}
+            maxZoom={21}
+          />
           {numberOfPolygons.map(item => (
             <TileLayer
               key={item}
-              url={`https://github.com/holiaka/chornobyl-polygon-tiles/blob/main/rgb-chm/chm-p${item}/{z}/{x}/{y}.webp?raw=true`}
+              url={`https://raw.githubusercontent.com/holiaka/chornobyl-polygon-tiles/main/orto/orto-p${item}/{z}/{x}/{y}.webp`}
               tms="true"
+              minZoom={10}
+              maxZoom={21}
             />
           ))}
-        
+        </LayerGroup>
+      </LayersControl.Overlay><LayersControl.Overlay name="Canopy height model (CHM)">
+        <LayerGroup>
+          <TileLayer
+            attribution='&copy; <a href="https://www.researchgate.net/profile/Dmytrii-Holiaka">Dmytrii Holiaka</a> '
+            url="https://raw.githubusercontent.com/holiaka/chornobyl-polygon-tiles/main/rgb-chm/chm-p1/{z}/{x}/{y}.webp"
+            tms="true"
+            minZoom={10}
+            maxZoom={21}
+          />
+          {numberOfPolygons.map(item => (
+            <TileLayer
+              key={item}
+              url={`https://raw.githubusercontent.com/holiaka/chornobyl-polygon-tiles/main/rgb-chm/chm-p${item}/{z}/{x}/{y}.webp`}
+              tms="true"
+              minZoom={10}
+              maxZoom={21}
+            />
+          ))}
+        </LayerGroup>
       </LayersControl.Overlay>
     </>
   );
